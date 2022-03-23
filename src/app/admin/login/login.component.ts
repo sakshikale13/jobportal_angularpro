e61fc540bf0c89c1d982c587f09c3e55123177e2
sakshi <kalesakshi1330@gmail.com>
2022-03-23 17:39:19 +0530

    second

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'src/app/cookie.service';
+import { ApiService } from 'src/app/api.service';
+import { FormControl } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +12,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user:any;
 
-  constructor() { }
+  constructor(private router: Router, private api: ApiService, private cookie: CookieService) { }
 
   ngOnInit(): void {
-  }
+    this.user = new FormGroup({
+      email: new FormControl("", Validators.compose([Validators.required])),
+      password: new FormControl("", Validators.compose([Validators.required]))
 
+
+    })
+
+  }
+  submit = (user: any) => {
+
+    // this.router.navigate(["/admin/dashboard"]);
+    let apiurl = "/admin/login";
+    let data = this.api.post(apiurl,
+      {
+        data: user
+      });
+    data.subscribe((mydata: any) => {
+      console.log(mydata);
+      console.log(data);
+
+
+      if (mydata.data.status == "success") {
+        this.cookie.set("usertype", "admins");
+        this.cookie.set("adminid",mydata.data.id);
+        this.cookie.set("name",mydata.data.name);
+        this.cookie.set("authkey", mydata.data.authkey);
+        this.router.navigate(["/admin/dashboard"]);
+
+      }
+      else {
+        alert("invalid email or password")
+      }
+    });
+
+
+  }
 }
--- src/app/cookie.service.ts
+++ src/app/cookie.service.ts
@@ -6,4 +6,29 @@ import { Injectable } from '@angular/core';
 export class CookieService {
 
   constructor() { }
-}
+  set=(name:string,value:string)=>{
+    var exdate = new Date();
+    exdate.setDate(exdate.getDate() + 10);
+    document.cookie = name + "=" + (value) + ";expires=" + exdate;
+
+  }
+  get=(name:string)=>{
+    let cookie = document.cookie;
+    let cookies = cookie.split(";");
+    for(let i = 0; i < cookies.length; i++)
+    {
+      let mycookie = cookies[i].split('=');
+      if(mycookie[0].trim() == name)
+        return mycookie[1];
+    }
+    return "";
+
+  }
+  delete=(name:string)=>{
+    var exdate = new Date();
+    exdate.setDate(exdate.getDate() - 10);
+    document.cookie = name + "=" + ("") + ";expires=" + exdate;
+
+
+  }
+  }
